4d626df78e0b0381b974ddf1b5f85a4f4f5756b9
Ahmed Mansour <ahmed@mansour.fi>
2018-12-16 21:26:03 +0200

    add addPageViewEvent functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { DataLayerService } from "./data-layer.service";
+import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = "Tour of Heroes";
+
+  constructor(router: Router, dataLayerService: DataLayerService) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        dataLayerService.addPageViewEvent(event.url);
+      }
+    });
+  }
 }
